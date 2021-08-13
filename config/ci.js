7a34c78ded066f92268871ae95a68f5cdc78d113
farrukh806 <farrukh806@gmail.com>
2021-08-13 22:09:20 +0530

    Travis ci added

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+	googleClientID:
+		'775775002020-v5m50tvb85976v1ashk0mmgrqjs4osa1.apps.googleusercontent.com',
+	googleClientSecret: '6zdUeEaPkAfOQdBe1n8v_D_L',
+	mongoURI: 'mongodb://127.0.0.1:27017/node_starter_ci',
+	cookieKey: '123123123',
+	redisUrl: 'redis://127.0.0.1:6379',
+};
---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+	googleClientID:
+		'775775002020-v5m50tvb85976v1ashk0mmgrqjs4osa1.apps.googleusercontent.com',
+	googleClientSecret: '6zdUeEaPkAfOQdBe1n8v_D_L',
+	mongoURI: 'mongodb://localhost/node_starter',
+	cookieKey: '123123123',
+	redisUrl: 'redis://127.0.0.1:6379',
+};
