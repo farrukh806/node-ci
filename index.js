848c98c8b1138a224a5174604bb48179abdccf39
farrukh806 <farrukh806@gmail.com>
2021-08-14 09:22:09 +0530

    Testing completed in production level also

--- index.js
+++ index.js
@@ -41,5 +41,6 @@ if (['production', 'ci'].includes(process.env.NODE_ENV)) {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+	console.log(process.env.NODE_ENV);
 	console.log(`Listening on port`, PORT);
 });
