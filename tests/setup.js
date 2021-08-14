1df79c1900cf562773cb4ad9fc73e9a86e66eb58
farrukh806 <farrukh806@gmail.com>
2021-08-14 10:28:19 +0530

    sleep time increased

--- tests/setup.js
+++ tests/setup.js
@@ -5,5 +5,4 @@ const keys = require('../config/keys');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
-console.log('NODE_ENV', process.env.NODE_ENV);
 jest.setTimeout(40000);
